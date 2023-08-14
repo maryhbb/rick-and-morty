c6e1eb0ee306f2ac03fae6a3b194ee604f31f158
Mary <76174247+maryhbb@users.noreply.github.com>
2023-08-15 00:47:14 +0200

    Implement Pagination for Character Display

--- index.js
+++ index.js
@@ -10,18 +10,32 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
+let maxPage = 1;
+let page = 1;
 const searchQuery = "";
 
 async function fetchCharacters() {
   try {
-    const response = await fetch("https://rickandmortyapi.com/api/character");
-    const { results } = await response.json();
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character?page=${page}`
+    );
+    const { info, results } = await response.json();
+    maxPage = info.pages;
+    pagination.textContent = `${page} / ${info.pages}`;
+
     results.forEach(createCharacterCard);
   } catch (error) {
     console.error("An error occurred while fetching characters:", error);
   }
 }
+
+prevButton.addEventListener("click", () => {
+  page > 1 && page--;
+  fetchCharacters();
+});
+nextButton.addEventListener("click", () => {
+  page < maxPage && page++;
+  fetchCharacters();
+});
 
 fetchCharacters();
